18212a1c4b599e1b81830033495d179772550871
Oleg Shilov <me@olegshilov.com>
2018-11-26 16:07:43 +0300

    chore: update some features at `postcss-preset-env` default config
    remove `postcss-color-mod-function` cuz it's already a part of `postcss-preset-env`
    
    Signed-off-by: Oleg Shilov <me@olegshilov.com>

--- index.js
+++ index.js
@@ -2,72 +2,71 @@
  * Copyright 2018 dialog LLC <info@dlg.im>
  */
 
-const postcss = require("postcss");
-const defaultsDeep = require("lodash.defaultsdeep");
+const postcss = require('postcss');
+const defaultsDeep = require('lodash.defaultsdeep');
 
 const defaultOptions = {
   import: {
-    skipDuplicates: true
+    skipDuplicates: true,
   },
   mixins: true,
-  color: true,
   env: {
     stage: 3,
-    browsers: "Chrome >= 45, ff >= 40, ie >= 10, Safari >= 8",
+    browsers: 'Chrome >= 45, ff >= 40, ie >= 10, Safari >= 8',
     autoprefixer: true,
     features: {
-      "custom-media-queries": true,
-      "custom-properties": true,
-      "all-property": true
-    }
+      'custom-media-queries': true,
+      'custom-properties': {
+        preserve: false,
+      },
+      'all-property': {
+        reset: 'all',
+      },
+      'nesting-rules': true,
+      'color-mod-function': true,
+    },
   },
   modules: false,
-  dedupe: false,
+  dedupe: true,
   rtl: false,
   report: true,
-  debug: false
+  debug: false,
 };
 
-const plugin = postcss.plugin("postcss-dialog", _options => {
+const plugin = postcss.plugin('postcss-dialog', (_options) => {
   const options = defaultsDeep({}, _options, defaultOptions);
   const plugins = [];
 
   if (options.import) {
-    plugins.push(require("postcss-import")(options.import));
+    plugins.push(require('postcss-import')(options.import));
   }
 
   if (options.mixins) {
-    plugins.push(require("postcss-mixins")(options.mixins));
-  }
-
-  if (options.color) {
-    plugins.push(require("postcss-color-mod-function")(options.color));
+    plugins.push(require('postcss-mixins')(options.mixins));
   }
 
   if (options.env) {
-    plugins.push(require("postcss-preset-env")(options.env));
-  }
-
-  if (options.modules) {
-    plugins.push(require("postcss-modules")(options.modules));
+    plugins.push(require('postcss-preset-env')(options.env));
   }
 
   if (options.dedupe) {
-    plugins.push(require("postcss-discard-duplicates")(options.dedupe));
+    plugins.push(require('postcss-discard-duplicates')(options.dedupe));
+  }
+
+  if (options.modules) {
+    plugins.push(require('postcss-modules')(options.modules));
   }
 
   if (options.rtl) {
-    plugins.push(require("rtlcss")(options.rtl));
+    plugins.push(require('rtlcss')(options.rtl));
   }
 
   if (options.report) {
-    plugins.push(require("postcss-reporter")(options.report));
+    plugins.push(require('postcss-reporter')(options.report));
   }
 
   if (options.debug) {
-    plugins.push(
-      require("postcss-browser-reporter")(options.debug)
-    );
+    plugins.push(require('postcss-browser-reporter')(options.debug));
   }
 
   return postcss(plugins);
